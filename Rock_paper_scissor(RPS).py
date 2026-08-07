96d656273dacd217c19018f0797c567c41c70d80
VISHAL108-Mech <vy4122000@gmail.com>
2026-08-07 11:30:24 +0530

    rps update2

--- Rock_paper_scissor(RPS).py
+++ Rock_paper_scissor(RPS).py
@@ -36,8 +36,14 @@ user_choice= int(input('''What do you choose? Enter:
 3 for Scissors
 \n'''))
 
+if user_choice == 1:
+    print("You Chose: Rock")
+elif user_choice == 2:
+    print("You Chose: Paper")
+elif user_choice == 3:
+    print("You Chose: Scissors")
+
 if 1 <= user_choice <= 3:
-    print(f"You Chose: {user_choice}")
     if user_choice == 1:
         print(rock)
     elif user_choice == 2:
@@ -46,9 +52,14 @@ if 1 <= user_choice <= 3:
         print(scissors)
     
     # Bot's Choice
-    print("Bot chose:")
-
     bot_choice = random.randint(1, 3)
+
+    if bot_choice == 1:
+        print("Bot Chose: Rock")
+    elif bot_choice == 2:
+        print("Bot Chose: Paper")
+    elif bot_choice == 3:
+        print("Bot Chose: Scissors")
 
     if bot_choice == 1:
         print(rock)
@@ -59,15 +70,15 @@ if 1 <= user_choice <= 3:
 
     # Game Logic
     if user_choice == bot_choice:
-        print("It's a draw.")
+        print("It's a draw.\n")
     elif user_choice == 1 and bot_choice == 3:
-        print("You Won!")
+        print("You Won!\n")
     elif user_choice == 2 and bot_choice == 1:
-        print("You Won!")
+        print("You Won!\n")
     elif user_choice == 3 and bot_choice == 2:
-        print("You Won!")
+        print("You Won!\n")
     else:
-        print("You Loose!")
+        print("You Loose!\n")
 else:
     print("Please enter a valid choice.")
     print("Run the game again for a retry.")
